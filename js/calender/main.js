db5b73d5edafe6f57f1a979f89baa25a5cf5302e
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-26 17:55:17 +0900

    달력만들기 start

---
+++ js/calender/main.js
@@ -0,0 +1,12 @@
+const date = new Date()
+const year = date.getFullYear()
+const month = date.getMonth() + 1
+const day = date.getDate()
+const dayOfWeek = date.getDay()
+
+console.log(date)
+
+console.log(year)
+console.log(month)
+console.log(day)
+console.log(dayOfWeek)
